14be09e4bda546a064199efd6c6d802d84b2d88e
GeorgeKrs <gkoursoumis97@gmail.com>
2021-09-16 09:54:12 +0300

    Vitals Form validation and API Call done, TODO modals

--- src/Forms/VitalsForm.js
+++ src/Forms/VitalsForm.js
@@ -5,27 +5,6 @@ import { db } from "../utils/firebase";
 import { addDoc, Timestamp, collection } from "firebase/firestore";
 
 const VitalsForm = () => {
-  const [form, setForm] = useState([
-    { systolic: "" },
-    { diastolic: "" },
-    { pulses: "" },
-    { temperature: "" },
-    { oxygen: "" },
-    { sugar: "" },
-    { weight: "" },
-  ]);
-
-  const [errors, setError] = useState([
-    { erSystolic: "" },
-    { erDiastolic: "" },
-    { erPulses: "" },
-    { erTemperature: "" },
-    { erOxygen: "" },
-    { erSugar: "" },
-    { erWeight: "" },
-    { erComments: "" },
-  ]);
-
   const [systolic, setSystolic] = useState("");
   const [diastolic, setDiastolic] = useState("");
   const [pulses, setPulses] = useState("");
@@ -35,20 +14,76 @@ const VitalsForm = () => {
   const [weight, setWeight] = useState("");
   const [comments, setComments] = useState("");
 
+  const [erSystolic, setErSystolic] = useState("");
+  const [erDiastolic, setErDiastolic] = useState("");
+  const [erPulses, setErPulses] = useState("");
+  const [erTemperature, setErTemperature] = useState("");
+  const [erOxygen, setErOxygen] = useState("");
+  const [erSugar, setErSugar] = useState("");
+  const [erWeight, setErWeight] = useState("");
+
   const [loading, setLoading] = useState(false);
+
+  const ValidateSystolic = (systolic) => {
+    if ((systolic < 50 || systolic > 220) && systolic !== "") {
+      setErSystolic("Τιμή εκτός ορίων. 50 < Συστολική Πίεση < 220.");
+    } else {
+      setErSystolic("");
+    }
+  };
+
+  const ValidateDiastolic = (diastolic) => {
+    if ((diastolic < 30 || diastolic > 115) && diastolic !== "") {
+      setErDiastolic("Τιμή εκτός ορίων. 30 < Διαστολική Πίεση < 115.");
+    } else {
+      setErDiastolic("");
+    }
+  };
+
+  const ValidatePulses = (pulses) => {
+    if ((pulses < 40 || pulses > 220) && pulses !== "") {
+      setErPulses("Τιμή εκτός ορίων. 40 < Παλμοί < 220.");
+    } else {
+      setErPulses("");
+    }
+  };
+
+  const ValidateTemperature = (temperature) => {
+    if ((temperature < 34 || temperature > 43) && temperature !== "") {
+      setErTemperature("Τιμή εκτός ορίων. 34 < Θερμοκρασία < 43");
+    } else {
+      setErTemperature("");
+    }
+  };
+
+  const ValidateOxygen = (oxygen) => {
+    if ((oxygen < 70 || oxygen > 100) && oxygen !== "") {
+      setErOxygen("Τιμή εκτός ορίων. 70 < Οξυγόνο (%)< 100");
+    } else {
+      setErOxygen("");
+    }
+  };
+
+  const ValidateSugar = (sugar) => {
+    if ((sugar < 85 || sugar > 350) && sugar !== "") {
+      setErSugar("Τιμή εκτός ορίων. 85 < Σάκχαρο < 350");
+    } else {
+      setErSugar("");
+    }
+  };
+
+  const ValidateWeight = (weight) => {
+    if ((weight < 30 || weight > 300) && weight !== "") {
+      setErWeight("Τιμή εκτός ορίων. 30 < Κιλά < 300");
+    } else {
+      setErWeight("");
+    }
+  };
 
   const FormHandler = () => {
     setLoading(true);
 
-    // console.log(systolic);
-    // console.log(diastolic);
-    // console.log(pulses);
-    // console.log(temperature);
-    // console.log(oxygen);
-    // console.log(diastolic);
-    // console.log(sugar);
-    // console.log(weight);
-    // console.log(comments);
+    let isEmpty = true;
 
     if (
       systolic === "" &&
@@ -59,27 +94,42 @@ const VitalsForm = () => {
       sugar === "" &&
       weight === ""
     ) {
-      alert("all empty");
+      alert("all empty modal");
     } else {
-      alert("at least 1 value");
+      isEmpty = false;
     }
-    // (async () => {
-    //   await addDoc(collection(db, "vitalsRecords"), {
-    //     email: "gkoursoumis97@gmail.com",
-    //     systolic: form.systolic,
-    //     diastolic: form.diastolic,
-    //     temperature: form.temperature,
-    //     oxygen: form.oxygen,
-    //     sugar: form.sugar,
-    //     weight: form.weight,
-    //     comments: form.comments,
-    //     SubmitDate: Timestamp.fromDate(new Date()),
-    //   });
-    // })();
+
+    if (
+      isEmpty === false &&
+      erSystolic === "" &&
+      erDiastolic === "" &&
+      erPulses === "" &&
+      erTemperature === "" &&
+      erOxygen === "" &&
+      erSugar === "" &&
+      erWeight === ""
+    ) {
+      alert("API CALL success modal");
+      (async () => {
+        await addDoc(collection(db, "vitalsRecords"), {
+          userEmail: "gkoursoumis97@gmail.com",
+          systolic: systolic,
+          diastolic: diastolic,
+          temperature: temperature,
+          oxygen: oxygen,
+          sugar: sugar,
+          weight: weight,
+          comments: comments,
+          SubmitDate: Timestamp.fromDate(new Date()),
+        });
+      })();
+    } else {
+      alert("API CALL FAILED modal");
+    }
 
     setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 500);
   };
 
   return (
@@ -94,10 +144,9 @@ const VitalsForm = () => {
               placeholder="Μεγάλη Πίεση"
               value={systolic}
               onChange={(e) => setSystolic(e.target.value)}
+              onInput={(e) => ValidateSystolic(e.target.value)}
             />
-            {errors.erSystolic ? (
-              <ErrorMsg ErrorMsg={errors.erSystolic}></ErrorMsg>
-            ) : null}
+            {erSystolic ? <ErrorMsg ErrorMsg={erSystolic}></ErrorMsg> : null}
           </div>
           <div className="col-sm-12 col-lg-6 mt-4">
             <label className="label">Διαστολική Πίεση (mmHg)</label>
@@ -107,10 +156,9 @@ const VitalsForm = () => {
               placeholder="Μικρή Πίεση"
               value={diastolic}
               onChange={(e) => setDiastolic(e.target.value)}
+              onInput={(e) => ValidateDiastolic(e.target.value)}
             />
-            {errors.erDiastolic ? (
-              <ErrorMsg ErrorMsg={errors.erDiastolic}></ErrorMsg>
-            ) : null}
+            {erDiastolic ? <ErrorMsg ErrorMsg={erDiastolic}></ErrorMsg> : null}
           </div>
         </div>
 
@@ -122,10 +170,9 @@ const VitalsForm = () => {
               className="inputValues"
               value={pulses}
               onChange={(e) => setPulses(e.target.value)}
+              onInput={(e) => ValidatePulses(e.target.value)}
             />
-            {errors.erPulses ? (
-              <ErrorMsg ErrorMsg={errors.erPulses}></ErrorMsg>
-            ) : null}
+            {erPulses ? <ErrorMsg ErrorMsg={erPulses}></ErrorMsg> : null}
           </div>
           <div className="col-sm-12 col-lg-6 mt-4">
             <label className="label">Θερμοκρασία (&#176;C)</label>
@@ -135,9 +182,10 @@ const VitalsForm = () => {
               className="inputValues"
               value={temperature}
               onChange={(e) => setTemperature(e.target.value)}
+              onInput={(e) => ValidateTemperature(e.target.value)}
             />
-            {errors.erTemperature ? (
-              <ErrorMsg ErrorMsg={errors.erTemperature}></ErrorMsg>
+            {erTemperature ? (
+              <ErrorMsg ErrorMsg={erTemperature}></ErrorMsg>
             ) : null}
           </div>
         </div>
@@ -151,10 +199,9 @@ const VitalsForm = () => {
               className="inputValues"
               value={oxygen}
               onChange={(e) => setOxygen(e.target.value)}
+              onInput={(e) => ValidateOxygen(e.target.value)}
             />
-            {errors.erOxygen ? (
-              <ErrorMsg ErrorMsg={errors.erOxygen}></ErrorMsg>
-            ) : null}
+            {erOxygen ? <ErrorMsg ErrorMsg={erOxygen}></ErrorMsg> : null}
           </div>
           <div className="col-sm-12 col-lg-6 mt-4"></div>
         </div>
@@ -167,10 +214,9 @@ const VitalsForm = () => {
               className="inputValues"
               value={sugar}
               onChange={(e) => setSugar(e.target.value)}
+              onInput={(e) => ValidateSugar(e.target.value)}
             />
-            {errors.erSugar ? (
-              <ErrorMsg ErrorMsg={errors.erSugar}></ErrorMsg>
-            ) : null}
+            {erSugar ? <ErrorMsg ErrorMsg={erSugar}></ErrorMsg> : null}
           </div>
           <div className="col-sm-12 col-lg-6 mt-4">
             <label className="label">Βάρος (Κιλά)</label>
@@ -180,10 +226,9 @@ const VitalsForm = () => {
               className="inputValues"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
+              onInput={(e) => ValidateWeight(e.target.value)}
             />
-            {errors.erWeight ? (
-              <ErrorMsg ErrorMsg={errors.erWeight}></ErrorMsg>
-            ) : null}
+            {erWeight ? <ErrorMsg ErrorMsg={erWeight}></ErrorMsg> : null}
           </div>
         </div>
 
